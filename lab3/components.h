6cbe56018c0bbfd736fe2e0579fb4a7e6d0284bb
Ashish Sonone <ashson07@gmail.com>
2014-01-28 21:35:16 +0530

    updated defs of neuron & edge

--- lab3/components.h
+++ lab3/components.h
@@ -7,32 +7,47 @@
 #include <ctime>
 using namespace std;
 
+typedef struct {
+    char layer_id; // {A-Z}
+    int seq_no; //order within the layer
+} neuron_id;
+
 /** class declaration **/
 class Edge;
 class Neuron;
 
 /** class definition **/
 class Neuron{
+    char type; // {'I', 'H', 'O'}
+    neuron_id id;
+    float delta_error; //delta-j
+    float signal_output;
 public:
     vector<Edge*> inputs; // input edges
     vector<Edge*> outputs; // output edges
-    Neuron(int in_size, int out_size);
+    Neuron(char n_type, neuron_id nid, int in_size, int out_size);
     ~Neuron();
-    void trigger(); // collect weighted input from input edges, calculate the output and set it to all output edges
+    void propogate_signal(); // collect weighted input from input edges, calculate the output and set it to all output edges
+    void propogate_error();  // collect weighted delta_errors from output edges, calculate self delta_error and propogate it to input edges
+    void update_weights(); // update weights of each 
 };
 
 class Edge{
     float weight; // weight on the edge
-    float input; // input at start end
-    //Neuron * start, *end; // contain the starting and ending peceptrons which it connects
+    float input_signal; // lower layer to high
+    float input_error; // high layer to low
 public:
     Edge(float weight);
     Edge();
     ~Edge();
-    void set_input(float x);
+    void set_signal(float x);
+    void set_error(float x);
     void set_weight(float x);
     float get_weight();
-    float get_output(); //calculates the output using weight * input
+    float get_input_signal();
+    float get_input_error();
+    float get_output_signal(); //calculates the output using weight * input_signal
+    float get_output_error(); 
     void print();
 };
 
