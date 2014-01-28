bb4b1f4ab7f8546805bf6f61720240ea8f338850
Himanshu Roy <himanshuroy@Himanshus-MacBook-Pro.local>
2014-01-28 23:20:30 +0530

    layer header :forced  vector use instead of array

--- lab3/layer.h
+++ lab3/layer.h
@@ -1,7 +1,14 @@
 #ifndef _LAYER
 #define _LAYER
 
+#include <iostream>
+#include <stdio.h>  
+#include <cmath>
+#include <vector>
+
 #include "components.h"
+
+
 
 class layer;
 
@@ -9,10 +16,10 @@ class layer {
 public:
 	char type;					// I , H, O  are the allowed types
 	char layer_id;				// this is the id of the layer
-	int population;			// number of nodes in the layer
-	int prev_population;	// population of previous layer
-	int next_population;	// population of next layer
-	Neuron * nodeList;		// array of nodes
+	int population;				// number of nodes in the layer
+	int prev_population;		// population of previous layer
+	int next_population;		// population of next layer
+	vector<Neuron*> neuronList;			// array of nodes
 
 	layer(char, char, int, int, int);		// constructor : type, layer_id, population, prev_population, next_layer
 	~layer()
