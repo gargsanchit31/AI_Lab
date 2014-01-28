a4eff58f40a0f299ef5d83212cb9953ed88f5a85
Himanshu Roy <himanshuroy@Himanshus-MacBook-Pro.local>
2014-01-28 22:46:21 +0530

    layer header #nodes of next/previous layers included

--- lab3/layer.h
+++ lab3/layer.h
@@ -10,9 +10,11 @@ public:
 	char type;					// I , H, O  are the allowed types
 	char layer_id;				// this is the id of the layer
 	int population;			// number of nodes in the layer
+	int prev_population;	// population of previous layer
+	int next_population;	// population of next layer
 	Neuron * nodeList;		// array of nodes
 
-	layer(char, char, int);		// constructor : type, layer_id, population
+	layer(char, char, int, int, int);		// constructor : type, layer_id, population, prev_population, next_layer
 	~layer()
 
 private:
