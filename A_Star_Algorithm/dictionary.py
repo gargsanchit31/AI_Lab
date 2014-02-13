9f60aa85b6e210b4b18d04c6915fb06c1bfe658c
himanshuroy <himanshuroy@himanshuroy-laptop.(none)>
2014-02-14 01:07:26 +0530

    writing make_vector script almost.....

--- A_Star_Algorithm/dictionary.py
+++ A_Star_Algorithm/dictionary.py
@@ -1,23 +1,30 @@
 #!/usr/bin/python
 
 from nltk.corpus import wordnet
-
+from nltk.tag import pos_tag
+from nltk.stem.wordnet import WordNetLemmatizer
 #if not wordnet.synsets(word_to_test):
   #Not an English Word
 #else:
   #English Word
 
-
-
 import sys
-#import en
+import re
 
+#function to find unique elements in a list
 def unique_list(l):
 	ulist = []
 	[ulist.append(x) for x in l if x not in ulist]
 	return ulist
 
-deleters = ('#','@','&')
+def remove_extras(l):
+	new_list = []
+	new_tagged_list = pos_tag(l)
+
+deleters = ('#','@','&','0','1','2','3','4','5','6','7','8','9')
+extras = ('"','.','(',')','-','/','\\')
+remove_poslist = ('NN','TO','IN','PRP','WP','SYM','RP','POS','PDT','NNP','NNPS','NNS','LS','FW','DT','CD',':',',','$','(',')','--','.')
+shred = ('"','*','@','(')
 
 input_file_name=sys.argv[1]
 output_file_name = sys.argv[2]
@@ -25,24 +32,55 @@ output_file_name = sys.argv[2]
 filein = open(input_file_name, "r")
 fileout = open(output_file_name,"w")
 
-b=[]
-a = ""
-for a in filein:
-	b.extend(unique_list(a.split(" ")))
+#take input and split into sentences
+list_of_sentences=[]
+inputlist2 = ""
+templist1 = []
+for inputline in filein:
+	templist1 = inputline.split('\t',1)
+	inputlist2 = templist1[1].split()
+	for word in inputlist2:
+		for element in shred:
+			if element in word and word in templist1 :
+				word = re.sub('[?*=|:();,!@#$.\"^]', '', word)
+				print word
+				print "        asdf"
+				print re.sub(r"[?*=|:();,!@#$.\"^]", r"", word)
+	tagged_sent = pos_tag(inputlist2)
+	templist2 = [word for word,pos in tagged_sent if pos not in remove_poslist ]
+	#templist1 = unique_list(inputline2.split())
+	#templist1.remove(templist1[0])
+	list_of_sentences.extend(templist2)
 
-print b
+#print list_of_sentences
+new_list_of_sentences=[]
+#for sentence in list_of_sentences:
+#	sentence.extend
 
-l = ""
-for l in b:
-	if l.startswith(deleters):
-		print ""
-	elif len(l) <= 2 :
-		print ""
-	elif "http" in l :
-		print ""
-	elif "ing" in l :
-		print ""
-	elif "ed" in l :
-		print ""
+#remove null strings from the list of sentences
+while '' in list_of_sentences:
+	list_of_sentences.remove('')
+
+list_of_sentences.sort()
+#now remove unnecessary words
+templist = ""
+for templist in list_of_sentences:
+	if templist.startswith(deleters):
+		list_of_sentences.remove(templist)
+	elif len(templist) <= 2:
+		list_of_sentences.remove(templist)
+	elif "http" in templist :
+		list_of_sentences.remove(templist)
+	elif templist[0].isupper():
+		list_of_sentences.remove(templist)
 	else:
-		print l
+		for element in extras:
+			if element in templist and templist in list_of_sentences:
+				list_of_sentences.remove(templist)
+				#print templist
+		for element in deleters:
+			if element in templist and templist in list_of_sentences:
+				list_of_sentences.remove(templist)
+				#print templist
+		fileout.write(templist.lower())
+		fileout.write("\n")
--- A_Star_Algorithm/nlkt_TEST.py
+++ A_Star_Algorithm/nlkt_TEST.py
@@ -6,7 +6,7 @@ from nltk.corpus import wordnet
 from nltk.tag import pos_tag
 from nltk.stem.wordnet import WordNetLemmatizer
 
-remove_poslist = ('WP','NN','TO','PRP','WP','MD','SYM','RP','POS','PDT','NNP','NNPS','NNS','LS','FW','DT','CD',':',',','$','(',')','--','.')
+remove_poslist = ('WP','TO','NN','PRP','WP','MD','SYM','RP','POS','PDT','NNP','NNPS','NNS','LS','FW','DT','CD',':',',','$','(',')','--','.')
 lmtzr = WordNetLemmatizer()
 
 input_file_name=sys.argv[1]
@@ -28,10 +28,11 @@ for words in old_list:
 	new_list.append(lmtzr.lemmatize(words))
 
 tagged_sent = pos_tag(new_list)
-new_list = [word.lower() for word,pos in tagged_sent if pos not in remove_poslist ]
+
+new_list = [word.lower() for word,pos in tagged_sent if pos not in remove_poslist or "itchy" in word]
 
 
-
+new_list.sort()
 for element in new_list:
 	if "'" not in element and len(element) > 1:
 		print element
@@ -39,9 +40,8 @@ for element in new_list:
 	else :
 		new_set = set(new_list)
 
-
-
-
+#for ele in tagged_sent:
+#	print ele
 
 #tagged_sent = pos_tag(corpus_list)
 	#print tagged_sent
