76f4ac87b1005d38e9afd81d72ae1e8cdde78acf
Sanchit Garg <garg.sanchitiitb@gmail.com>
2014-04-16 04:47:05 +0530

    circuit verification working Circuit Verification.odp has to be modified accordingly

--- prolog/palindrom.pl
+++ prolog/palindrom.pl
@@ -4,6 +4,13 @@ type(not1,not).
 type(not2,not).
 type(and1,and).
 
+type(x1,term).
+type(x2,term).
+type(x3,term).
+type(x4,term).
+type(x5,term).
+type(x6,term).
+
 connected(x1,in(1,xor1)).
 connected(x5,in(2,xor1)).
 connected(x2,in(1,xor2)).
@@ -19,33 +26,40 @@ connected(out(and1), x6).
 
 %============================
 
-signal(x1,1).
-signal(x2,0).
-signal(x3,1).
-signal(x4,1).
-signal(x5,1).
+signal(X1,X2,X3,X4,X5, Y) :- valid(X1,X2,X3,X4,X5,Y), signal(X1,X2,X3,X4,X5,x6,Y).
 
 % ===========================
 
 %AND gate
-signal(T, 1) :- connected(out(G), T), type(G, and), signal(in(1,G), Y1), signal(in(2,G),Y2), Y1 is 1, Y2 is 1, print(G), write(' : AND 1\n'), !.
-signal(T, 0) :- connected(out(G), T), type(G, and), print(G), write(' : AND 0\n'),!.
+signal(X1,X2,X3,X4,X5,T,1) :- connected(out(G), T), type(G, and), signal(X1,X2,X3,X4,X5,in(1,G),1), signal(X1,X2,X3,X4,X5,in(2,G),1).
+signal(X1,X2,X3,X4,X5,T,0) :- connected(out(G), T), type(G, and), \+ signal(X1,X2,X3,X4,X5,T,1).
 
 %OR gate
-signal(T, 0) :- connected(out(G), T), type(G, or), signal(in(1,G), Y1), signal(in(2,G),Y2), Y1 is 0, Y2 is 0, print(G), write(': OR 0\n'), !.
-signal(T, 1) :- connected(out(G), T), type(G, or), print(G), write(' : OR 1\n'), !.
+signal(X1,X2,X3,X4,X5,T,0) :- connected(out(G), T), type(G, or), signal(X1,X2,X3,X4,X5,in(1,G),0), signal(X1,X2,X3,X4,X5,in(2,G),0).
+signal(X1,X2,X3,X4,X5,T,1) :- connected(out(G), T), type(G, or), \+ signal(X1,X2,X3,X4,X5,T,0).
+
 
 %NOT gate
-signal(T, 0) :- connected(out(G), T), type(G, not), signal(in(1,G), Y1), Y1 is 1, print(G), write(' : NOT 0\n'), !.
-signal(T, 1) :- connected(out(G), T), type(G, not),print(G), write(' : NOT 1\n'),!.
+signal(X1,X2,X3,X4,X5,T, 0) :- connected(out(G), T), type(G, not), signal(X1,X2,X3,X4,X5,in(1,G),1).
+signal(X1,X2,X3,X4,X5,T, 1) :- connected(out(G), T), type(G, not), \+ signal(X1,X2,X3,X4,X5,T, 0).
 
 %XOR gate
-signal(T, 0) :- connected(out(G), T), type(G, xor),  signal(in(1,G), Y1), signal(in(2,G),Y2), equal(Y1,Y2), print(G), write(' : XOR 0\n'), !.
-signal(T, 1) :- connected(out(G), T), type(G, xor), signal(in(1,G), Y1), signal(in(2,G),Y2), \+ equal(Y1,Y2), print(G), write(' : XOR 1\n'), !.
+signal(X1,X2,X3,X4,X5,T,0) :- connected(out(G), T), type(G, xor), signal(X1,X2,X3,X4,X5,in(1,G),Y1), signal(X1,X2,X3,X4,X5,in(2,G),Y2), 								equal(Y1,Y2).
+signal(X1,X2,X3,X4,X5,T,1) :- connected(out(G), T), type(G, xor), \+ signal(X1,X2,X3,X4,X5,T,0).
 
 %direct if lhs of connected is terminal
-signal(T, 1) :- connected(T1, T), signal(T1, 1),print(T1), write(' : direct 1\n'), !. %case when signal(in(1,a1), S) called
-signal(T, 0) :- connected(T1, T), signal(T1, 0),print(T1), write(' : direct 0\n'), !. %case when signal(in(1,a1), S) called
+signal(X1,X2,X3,X4,X5,T, 1) :- connected(T1, T), type(T1, term), signal(X1,X2,X3,X4,X5,T1, 1). %case when signal(in(1,a1), S) called
+signal(X1,X2,X3,X4,X5,T, 0) :- connected(T1, T), type(T1, term), signal(X1,X2,X3,X4,X5,T1, 0). %case when signal(in(1,a1), S) called
+
+signal(X1,X2,X3,X4,X5,x1,X1) :- valid(X1,X2,X3,X4,X5,0).
+signal(X1,X2,X3,X4,X5,x2,X2) :- valid(X1,X2,X3,X4,X5,0).
+signal(X1,X2,X3,X4,X5,x3,X3) :- valid(X1,X2,X3,X4,X5,0).
+signal(X1,X2,X3,X4,X5,x4,X4) :- valid(X1,X2,X3,X4,X5,0).
+signal(X1,X2,X3,X4,X5,x5,X5) :- valid(X1,X2,X3,X4,X5,0).
 
 %check equality
 equal(X,X).
+
+%for fixing domain
+valid(X1,X2,X3,X4,X5,Y) :- valid(X1), valid(X2), valid(X3), valid(X4), valid(X5), valid(Y).
+valid(X) :- member(X,[0,1]).
